8a05de4b337f9cf79755c21959d794bcef9d44e0
Zaphkeil <lancex601@gmail.com>
2021-10-04 17:17:14 +0800

    link

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
-	addEventListener("click", (e) => {
-		window.location.href='sethost.php';
-  });
+var calculator = document.getElementById("calculatolink");
+
+calculator.onclick = function() {
+	window.location.href='https://zaphkeil.github.io/calculator/calculator/'
+};
