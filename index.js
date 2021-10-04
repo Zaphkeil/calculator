95a3d1b69ac879b6c10ad5c031406541022e6714
Zaphkeil <lancex601@gmail.com>
2021-10-04 18:16:13 +0800

    css change

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-var calculator = document.getElementById("div");
-oDiv.onclick = function(){
-    window.location.href='https://zaphkeil.github.io/calculator/calculator/';
-}
+var Div0 = document.getElementById("calculatorlink");
+Div0.addEventListener("click", function(){
+	 window.location.href = 'https://zaphkeil.github.io/calculator/calculator/';
+});
+    //window.location.href='https://zaphkeil.github.io/calculator/calculator/';
 
