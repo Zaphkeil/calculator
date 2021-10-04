49dc60e28e66ed4464532173d4202ae85dc0f2e4
zaphkeil <lancex601@gmail.com>
2021-10-05 00:00:24 +0800

    div click

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-var Div0 = document.getElementById("calculatorlink");
-Div0.addEventListener("click", function(){
-	 window.location.href = 'https://zaphkeil.github.io/calculator/calculator/';
+var calculatorlink= document.getElementsByClassName(".calculatorlink");
+
+calculatorlink.addEventListener('click', function (event) {
+    window.location.href='https://zaphkeil.github.io/calculator/calculator/';
 });
-    //window.location.href='https://zaphkeil.github.io/calculator/calculator/';
+
+//window.location.href='https://zaphkeil.github.io/calculator/calculator/';
 
