70517327ab6abea886456880d8b596212f03a69f
Zaphkeil <lancex601@gmail.com>
2021-10-10 13:06:37 +0800

    Div button fix

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-var calculatorlink = document.getElementById("calculatorlink");
-var githublink =document.getElementById("githublink")
+var calculatorlink = document.getElementById(".calculatorlink");
+var githublink =document.getElementById(".githublink")
 
 calculatorlink.addEventListener("click", function(){
 	 window.location.href = 'https://zaphkeil.github.io/calculator/calculator/';
 });
 
 githublink.addEventListener("click", function(){
-    window.location.href = 'https://github.com/Zaphkeil/calculator';
+    window.open.href = 'https://github.com/Zaphkeil/calculator';
 });
 
