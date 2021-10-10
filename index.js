9e9ae94c4e67fbc65103d5c220c13f3408be1122
Zaphkeil <lancex601@gmail.com>
2021-10-10 13:08:44 +0800

    Div button fix document

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-var calculatorlink = document.getElementById(".calculatorlink");
-var githublink =document.getElementById(".githublink")
+var calculatorlink = document.querySelector(".calculatorlink");
+var githublink =document.querySelector(".githublink")
 
 calculatorlink.addEventListener("click", function(){
 	 window.location.href = 'https://zaphkeil.github.io/calculator/calculator/';
