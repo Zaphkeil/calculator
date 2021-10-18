4082b982fc1d2d042c0b4a40094feacba0540463
kenshan1212 <ken20041208@gmail.com>
2021-10-18 11:57:17 +0800

    calculatorJs2

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const calculatorlink = document.querySelector(".calculatorlink");
 const randomlink = document.querySelector(".randomlink");
 const githublink = document.querySelector(".githublink");
+const calculator_Js2 = document.querySelector(".gcalculator_Js2link");
 
 calculatorlink.addEventListener("click", function(){
 	 window.location.href = 'https://zaphkeil.github.io/calculator/calculator/';
@@ -15,3 +16,6 @@ githublink.addEventListener("click", function(){
     window.open('https://github.com/Zaphkeil/calculator');
 });
 
+calculator_Js2link.addEventListener("click", function(){
+    window.open('https://github.com/Zaphkeil/testcalculator');
+});
