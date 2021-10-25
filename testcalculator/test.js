ea7001c53316e854d084819fef1f874d9111290d
kenshan1212 <ken20041208@gmail.com>
2021-10-25 12:02:30 +0800

    css2

--- testcalculator/test.js
+++ testcalculator/test.js
@@ -1,1 +1,8 @@
-const 
+const display1El = document.querySelector(".display-1");
+const display2El = document.querySelector(".display-2");
+const tempResultEl = document.querySelector(".temp-result");
+const numbersEl = document.querySelectorAll(".number");
+const operationEl = document.querySelectorAll(".op");
+const equalEl = document.querySelector(".equal");
+const clearAllEl = document.querySelector(".all-clear");
+const clearLastEl = document.querySelector(".last-entity-clear");
